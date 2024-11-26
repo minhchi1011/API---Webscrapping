6b98fb1d61bc37bdb6d5f6f2f83e6e19a2c21ddd
Hoang Minh Chi <155606014+minhchi1011@users.noreply.github.com>
2024-11-26 14:58:53 +0100

    Implement redirection from root to Swagger docs

--- TP2 and  3/services/epf-flower-data-science/main.py
+++ TP2 and  3/services/epf-flower-data-science/main.py
@@ -1,8 +1,12 @@
 import uvicorn
-
+from fastapi.responses import RedirectResponse
 from src.app import get_application
 
 app = get_application()
+
+@app.get("/")
+async def redirect_to_docs():
+    return RedirectResponse(url="/docs")
 
 if __name__ == "__main__":
     uvicorn.run("main:app", debug=True, reload=True, port=8080)
