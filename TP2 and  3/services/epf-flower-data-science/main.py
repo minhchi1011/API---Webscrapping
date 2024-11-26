ca4dceba0a036919c5aba74b17a56fd880ea1fc1
Hoang Minh Chi <155606014+minhchi1011@users.noreply.github.com>
2024-11-26 15:36:30 +0100

    use fastapi

--- TP2 and  3/services/epf-flower-data-science/main.py
+++ TP2 and  3/services/epf-flower-data-science/main.py
@@ -1,10 +1,11 @@
 import uvicorn
+from fastapi import FastAPI
 from fastapi.responses import RedirectResponse
 from src.app import get_application
 
-app = get_application()
+app: FastAPI = get_application()
 
-@app.get("/")
+@app.get("/", include_in_schema=False)
 async def redirect_to_docs():
     return RedirectResponse(url="/docs")
 
